53cd5f3cd171415ebb59cbf2deee3ef2d1ad0d10
Nadia Khoptiuk <holineinadia@gmail.com>
2022-08-19 10:20:29 +0300

    sort messages by date

--- src/components/MessagesHistory/MessagesHistory.js
+++ src/components/MessagesHistory/MessagesHistory.js
@@ -1,3 +1,4 @@
+import { compareAsc, format } from 'date-fns';
 import { useSelector } from 'react-redux';
 import { openedChat } from 'redux/chat/chatSelectors';
 import s from './MessageHistory.module.css';
@@ -5,24 +6,25 @@ import s from './MessageHistory.module.css';
 export default function MessagesHistory() {
   const selectedContact = useSelector(openedChat);
 
+  const messageList = [...selectedContact?.messages].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
   return (
     <ul>
-      {selectedContact?.messages?.map(
-        ({ message, isSendedByMe, date }, index) => {
-          const newDate = new Date(date).toLocaleString();
-          console.log(newDate);
+      {messageList.map(({ message, isSendedByMe, date }, index) => {
+        const newDate = format(new Date(date), 'PP, p');
 
-          return (
-            <li
-              key={index}
-              className={isSendedByMe ? s.myMessage : s.interlocutorMessage}
-            >
-              <p className={s.messageText}>{message}</p>
-              <span className={s.messageDate}>{newDate}</span>
-            </li>
-          );
-        }
-      )}
+        return (
+          <li
+            key={index}
+            className={isSendedByMe ? s.myMessage : s.interlocutorMessage}
+          >
+            <p className={s.messageText}>{message}</p>
+            <span className={s.messageDate}>{newDate}</span>
+          </li>
+        );
+      })}
     </ul>
   );
 }
